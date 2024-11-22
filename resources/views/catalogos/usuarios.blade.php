7c72e426cda3ff1f790532e38f799b237a5ba496
Guillermo Ricardo Garcia Garcia <86686479+RicardoGarci@users.noreply.github.com>
2024-11-22 12:55:24 -0600

    Update usuarios.blade.php
    cambios

--- resources/views/catalogos/usuarios.blade.php
+++ resources/views/catalogos/usuarios.blade.php
@@ -106,6 +106,25 @@
 <li class="side-nav-item">
     <a href="{{ route('catalogoContratistas.index') }}" class="side-nav-link">
         <i class="mdi mdi mdi-36px mdi-bank-plus"></i>
+        <span>Usuarios</span>
+    </a>
+</li>
+<li class="side-nav-item">
+    <a href="{{ route('catalogoContratistas.index') }}" class="side-nav-link">
+        <i class="mdi mdi mdi-36px mdi-bank-plus"></i>
+        <span>Usuarios</span>
+    </a>
+</li>
+<li class="side-nav-item">
+    <a href="{{ route('catalogoContratistas.index') }}" class="side-nav-link">
+        <i class="mdi mdi mdi-36px mdi-bank-plus"></i>
+        <span>Usuarios</span>
+    </a>
+</li>
+
+<li class="side-nav-item">
+    <a href="{{ route('catalogoContratistas.index') }}" class="side-nav-link">
+        <i class="mdi mdi mdi-36px mdi-bank-plus"></i>
         <span>catalogo prueba</span>
        
     </a>
