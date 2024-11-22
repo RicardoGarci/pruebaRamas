6b2bd20579c43396e67e9216d7925770501205ec
Guillermo Ricardo Garcia Garcia <86686479+RicardoGarci@users.noreply.github.com>
2024-11-22 12:33:46 -0600

    Update navigation.blade.php
    catalogo terminado

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -56,6 +56,12 @@
                     <span>Usuarios</span>
                 </a>
             </li>
+            <li class="side-nav-item">
+                <a href="{{ route('catalogoContratistas.index') }}" class="side-nav-link">
+                    <i class="mdi mdi mdi-36px mdi-bank-plus"></i>
+                    <span>catalogo prueba</span>
+                </a>
+            </li>
             {{-- seccion de solicitudes historico --}}
             <li class="side-nav-item">
                 <a href="" class="side-nav-link">
